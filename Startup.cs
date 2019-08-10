8520bfa10d98012c50e66bc5c3e02492728b5044
Faisal Mustafa <faisalm@microsoft.com>
2019-08-09 19:05:49 -0500

    Added Swagger Support

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using ProductApp.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ProductApp
 {
@@ -31,6 +32,11 @@ namespace ProductApp
 
             services.AddDbContext<ProductAppContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("ProductAppContext")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Simple Items API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +53,17 @@ namespace ProductApp
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple Items API");
+            });
+
             app.UseMvc();
         }
     }
